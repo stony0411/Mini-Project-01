05062e6748d8e4ec1c7e87590e7b991838131a81
stonywu-1663686 <stonywu@uw.edu>
2022-03-24 01:14:05 -0400

    verify saved model

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-## Mini-Project-01
-ResNets (or Residual Networks) are one of the most commonly used models for image classification
-tasks. In this project, you will design and train your own ResNet model for CIFAR-10 image
-classification. In particular, your goal will be to maximize accuracy on the CIFAR-10 benchmark
-while keeping the size of your ResNet model under some specified fix budget. Model size, typically
-measured as the number or trainable parameters, is important when models need to be stored on
-devices with limited storage capacity (e.g. IoT/edge devices).
+## Mini-Project-01 For ECE-7123 From Group 36
+"project1.ipynb" is the jupyter notebook document which contains all the training process code.
+"project1_model.pt" is the trained weight saved.
+"project1_model.py" contains the model structure of our resnet.
--- project1_model.py
+++ project1_model.py
@@ -2,7 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 class BasicBlock(nn.Module):
-
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
@@ -27,21 +26,19 @@ class BasicBlock(nn.Module):
         out = F.relu(out)
         return out
 
-
-
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
-        self.in_planes = 64
+        self.in_planes = 48
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+        self.conv1 = nn.Conv2d(3, 48, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512, num_classes)
+        self.bn1 = nn.BatchNorm2d(48)
+        self.layer1 = self._make_layer(block, 48, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 96, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 192, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 384, num_blocks[3], stride=2)
+        self.linear = nn.Linear(384, num_classes)
 
     def _make_layer(self, block, planes, num_blocks, stride):
         strides = [stride] + [1]*(num_blocks-1)
@@ -63,4 +60,4 @@ class ResNet(nn.Module):
         return out
 
 def project1_model():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
+    return ResNet(BasicBlock, [1, 2, 4, 1])
